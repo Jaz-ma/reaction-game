3324fdaed88bf3beaaff548db305bf6df24c311b
Jaz-ma <potatoorgy3@gmail.com>
2022-06-12 21:36:30 +0100

    init commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,35 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<h1>Reaction Game</h1>
+<button @click="startGame">Play</button>
+<Results/>
+<Block v-show="showBlock" @boxClicked="boxClicked"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Block from './components/Block.vue'
+import Results from './components/Results.vue'
 
 export default {
   name: 'App',
+  data(){
+    return{
+      showBlock:false,
+
+    }
+  },
   components: {
-    HelloWorld
+    Block,
+    Results,   
+  },
+  methods:{
+    startGame(){
+      setTimeout(()=>{
+        this.showBlock=true
+      },Math.random()*2000)
+    },
+    boxClicked(){
+      this.showBlock=false
+    }
   }
 }
 </script>
@@ -23,4 +43,15 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+h1{
+  color: rgb(16, 88, 28);
+}
+button{
+  padding: 1rem 2rem;
+  border-radius: 10px;
+  color: white;
+  font-size: 23px;
+  background: rgb(12, 100, 49);
+}
 </style>
+
---
+++ src/components/Block.vue
@@ -0,0 +1,30 @@
+<template>
+<div @click="boxClicked">
+    click me
+</div>
+</template>
+
+<script>
+export default {
+    methods:{
+        boxClicked(){
+            this.$emit('boxClicked')
+        }
+    }
+}
+</script>
+
+<style scoped>
+div{
+    padding: 11rem 10px;
+    color: white;
+    font-size: 23px;
+    background: green;
+    width: 32%;
+    float: center;
+    margin: auto;
+    margin-top: 2rem;
+    border-radius: 31px;
+}
+</style>
+
